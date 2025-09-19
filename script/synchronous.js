576edc39a0df1aec380f1b5a1569702fd39f8be3
Rasheed <hidrahtweet@gmail.com>
2025-09-19 22:30:19 +0100

    sync and async

---
+++ script/synchronous.js
@@ -0,0 +1,12 @@
+console.log(" I ");
+console.log(" eat ");
+console.log(" ice cream ");
+console.log(" with a ");
+console.log(" spoon ");
+
+
+console.log(" I ");
+console.log(" ice cream ");
+console.log(" eat ");
+console.log(" spoon ");
+console.log(" with a ");
